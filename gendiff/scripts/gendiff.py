52b54eb0e46f7e9de52480ed11a001a1df4c7698
VictorVangeli <whgaleon1@yandex.ru>
2024-10-23 20:47:22 +0300

    update 'gendiff.py'

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,8 +6,12 @@ def main():
                                      "Compares two configuration files and "
                                      "shows a difference.")
 
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
+    parser.add_argument('first_file',
+                        type=str)
+    parser.add_argument('second_file',
+                        type=str)
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
     args = parser.parse_args()
     # print(f"Comparing {args.first_file} with {args.second_file}")
 
