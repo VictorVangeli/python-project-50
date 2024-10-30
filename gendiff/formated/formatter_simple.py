46fedb27a405b3e17c029bb14ce189e545a875c1
VictorVangeli <whgaleon1@yandex.ru>
2024-10-31 01:23:17 +0300

    change typehint

--- gendiff/formated/formatter_simple.py
+++ gendiff/formated/formatter_simple.py
@@ -1,12 +1,12 @@
-from typing import Dict, List
+from typing import Dict, List, Any
 
 
-def format_diff_simple(diff: List[Dict[str, str]]) -> str:
+def format_diff_simple(diff: List[Dict[str, Any]]) -> str:
     """
     Formats the structured diff data into a simple plain text format.
 
     Args:
-        diff (List[Dict[str, str]]): The structured diff data.
+        diff (List[Dict[str, Any]]): The structured diff data.
 
     Returns:
         str: A formatted string representing the diff in a simple format.
