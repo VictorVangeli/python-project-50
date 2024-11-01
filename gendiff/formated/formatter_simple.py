7221e5e278f09d994dca180c9580211e89a0b3d2
VictorVangeli <whgaleon1@yandex.ru>
2024-11-01 12:19:05 +0300

    Update 'formatter_simple'

--- gendiff/formated/formatter_simple.py
+++ gendiff/formated/formatter_simple.py
@@ -1,4 +1,6 @@
-from typing import Any, Dict, List
+from typing import Any, Callable, Dict, List
+
+from gendiff.const import INDENT_DICT
 
 
 def format_diff_simple(diff: List[Dict[str, Any]]) -> str:
@@ -12,20 +14,29 @@ def format_diff_simple(diff: List[Dict[str, Any]]) -> str:
         str: A formatted string representing the diff in a simple format.
     """
     to_diff = []
+    formatter_functions = formatters_simple()
 
     for info in diff:
-        key = info["key"]
         type_ = info["type"]
+        formatted_text = formatter_functions[type_](info)
 
-        match type_:
-            case "added":
-                to_diff.append(f"+ {key}: {info['new_value']}")
-            case "removed":
-                to_diff.append(f"- {key}: {info['old_value']}")
-            case "changed":
-                to_diff.append(f"- {key}: {info['old_value']}")
-                to_diff.append(f"+ {key}: {info['new_value']}")
-            case "unchanged":
-                to_diff.append(f"  {key}: {info['value']}")
+        if isinstance(formatted_text, list):
+            to_diff.extend(formatted_text)
+        else:
+            to_diff.append(formatted_text)
 
     return "\n".join(to_diff)
+
+
+def formatters_simple() -> Dict[str, Callable[[Dict[str, Any]], str]]:
+    indent = INDENT_DICT.get("indent_size_for_simple")
+
+    return {
+        "added": lambda info: f"{indent}+ {info['key']}: {info['new_value']}",
+        "removed": lambda info: f"{indent}- {info['key']}: {info['old_value']}",
+        "changed": lambda info: [
+            f"{indent}- {info['key']}: {info['old_value']}",
+            f"{indent}+ {info['key']}: {info['new_value']}",
+        ],
+        "unchanged": lambda info: f"{indent}  {info['key']}: {info['value']}",
+    }
